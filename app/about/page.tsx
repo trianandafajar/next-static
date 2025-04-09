278ad682d3eb3ef5642132c0c6f0ac18f1bf3331
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-09 09:42:37 +0700

    Update About Pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,11 +2,14 @@ import Link from 'next/link'
 
 export default function About() {
   return (
-    <>
-      <h1>About</h1>
-      <div>
-        Back to <Link href="/">Homepage</Link>
-      </div>
-    </>
+    <main className="min-h-screen flex flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold mb-4">About</h1>
+      <p className="text-gray-700">
+        Back to{' '}
+        <Link href="/" className="text-blue-500 hover:underline">
+          Homepage
+        </Link>
+      </p>
+    </main>
   )
 }
